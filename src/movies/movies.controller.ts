7ac07d2a97e8386f2b1b369d96acc69476435b46
Minjeong Kim <h_hoh@naver.com>
2021-05-20 15:54:25 +0900

    #2.1 More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,24 +16,34 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id')
+  // search가 :id 밑에 있으면 id = 'search'로 판단
+  @Get('search')
+  serach(@Query('year') serachingYear: string) {
+    return `We are searching for a movie made after: ${serachingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This will delete a movie: ${movieId}`;
   }
 
   // 일부분만 업데이트 할 때
-  @Patch('/:id')
-  path(@Param('id') movieId: string) {
-    return `This will update a movie: ${movieId}`;
+  // express와 다른 점은 json으로 받기 용이하다
+  @Patch(':id')
+  path(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
